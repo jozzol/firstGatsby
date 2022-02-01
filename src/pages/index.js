805d32096b3feb30e0ac07a9edeba6b4a4a48682
jozzol <joseg1224@hotmail.com>
2022-02-01 11:21:25 -0500

    added image plugin, added 1 image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const HomePage = ()=>{
   return (
-    <Layout title='Index page' heading='Index'/>
+    <Layout title='Index page' heading='Index'>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/JXetxQh.jpeg"
+      />
+    </Layout>
   )
 };
 
