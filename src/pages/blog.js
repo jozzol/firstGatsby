4c0613baa8f6a2b950c88ec39f19d6529f04d0ee
jozzol <joseg1224@hotmail.com>
2022-02-03 14:25:36 -0500

    added mdx, added content to post, used mdx and grapql to render post on the blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 
 
@@ -9,8 +10,14 @@ const BlogPage = ({data}) => {
     <Layout heading="My Blog Posts">
       <ul>
           {
-              data.allFile.nodes.map(node => (
-                  <li key={node.name}>{node.name}</li>
+              data.allMdx.nodes.map(node => (
+                  <article key={node.id}>
+                    <h2>{node.frontmatter.title}</h2>
+                    <p>Posted: {node.frontmatter.date}</p>
+                    <MDXRenderer>
+                      {node.body}
+                    </MDXRenderer>
+                  </article>
               ))
           }
       </ul>
@@ -19,13 +26,19 @@ const BlogPage = ({data}) => {
 }
 // se le podria agregar un nombre a la query como tal que iria al inicio despues de grapql, con el fin de debuggin en caso de errores
 export const query = graphql`
-    query{
-    allFile {
-      nodes {
-        name
+query {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        date
+        title
       }
+      id
+      body
     }
-  }`
+  }
+}`
+
   
 
 export default BlogPage
