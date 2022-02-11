6ba7c070276c50451241de7cf14a49a8df6284c5
jozzol <joseg1224@hotmail.com>
2022-02-11 13:09:45 -0500

    tested adding a button from material UI

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import { container, Cheading, navlinks, navlinkitem, navlinktext, siteTitle, } from './layout.module.css';
+import Button from '@mui/material/Button';
 import '@fontsource/roboto/300.css';
 
 const Layout = ({title, heading, children}) => {
@@ -20,10 +21,12 @@ const Layout = ({title, heading, children}) => {
             <title>{data.site.siteMetadata.title}</title>
             <header className={siteTitle}>{data.site.siteMetadata.title}</header>
             <nav className={navlinks}>
+              <Button variant="outlined">
                 <li className={navlinkitem}><Link to='/' className={navlinktext}>Index</Link></li>
                 <li className={navlinkitem}><Link to='/about' className={navlinktext}>About</Link></li>
                 <li className={navlinkitem}><Link to='/contact' className={navlinktext}>Contact</Link></li>
                 <li className={navlinkitem}><Link to='/blog' className={navlinktext}>Blog</Link></li>
+              </Button>
             </nav>
             <h1 className={Cheading}>{heading}</h1>
             {children}
