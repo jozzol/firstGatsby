7893f0c02336ce1c97752c965be289b4f3af6768
jozzol <joseg1224@hotmail.com>
2022-04-20 16:55:40 -0500

    some changes to text

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout';
 const About = () =>{
     return(
         <Layout title='About us' heading='About'>
-            <p>First gatsby page using the tutorial and experimenting</p>
+            <p>simple page with gatsby using its core features and graphql</p>
         </Layout>
     )
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { StaticImage } from 'gatsby-plugin-image';
 const HomePage = ()=>{
   return (
     <Layout title='Index page' heading='Index'>
-      <p>Index page for first gatsby page</p>
+      <p>Simple page with gatsby</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="../images/dog1.jpg"
